e940531d305defc0c9997cd79679643ac27dbeee
Uyamikun <nabil.nabighah@gmail.com>
2021-01-05 11:08:34 +0700

    message handle

--- app.py
+++ app.py
@@ -37,11 +37,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    """ Here's all the messages will be handled and processed by the program """
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+    msg = event.message.text
+    if 'hello' in msg:
+        message = TextSendMessage(text='cobain')
+        line_bot_api.reply_message(event.reply_token,message)
+    else
+        message = TextSendMessage(text=msg)
+        line_bot_api.reply_message(event.reply_token,message)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
